1e6c0981328fbf1a82ea29eaa7610d68f08b32db
Vaghasiya Dev <150675017+Vaghasiya-Dev@users.noreply.github.com>
2026-09-24 21:18:34 +0530

    Update API_URL to use environment variable

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-const API_URL = 'http://localhost:8000/todos/';
+const API_URL = `${process.env.REACT_APP_API_URL}/todos/`;
 
 export function App() {
   const [todos, setTodos] = useState([]);

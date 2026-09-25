47d34659893c16eb004b3f3b8dfdd8a13cf54b9a
Vaghasiya Dev <150675017+Vaghasiya-Dev@users.noreply.github.com>
2026-09-25 12:15:18 +0530

    Accept Atlas MongoDB URI variable

--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 import json, os
 from pymongo import MongoClient
 
-mongo_uri = os.getenv('MONGO_URI') or (
+mongo_uri = os.getenv('MONGO_URI') or os.getenv('MONGODB_URI') or (
     'mongodb://' + os.environ['MONGO_HOST'] + ':' + os.environ['MONGO_PORT']
 )
 db = MongoClient(mongo_uri)['test_db']
